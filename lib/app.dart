8c29f3e55be3b6af52e0aebe372bac04925ec705
Hasan Mohsin <hasan@hasali.dev>
2025-09-14 15:17:01 +0100

    Add index and end totals to scoresheet

--- lib/app.dart
+++ lib/app.dart
@@ -8,9 +8,19 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Archery Toolkit',
-      theme: ThemeData(brightness: Brightness.light),
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: _buildTheme(Brightness.light),
+      darkTheme: _buildTheme(Brightness.dark),
       home: const SessionsPage(),
+    );
+  }
+
+  ThemeData _buildTheme(Brightness brightness) {
+    return ThemeData(
+      brightness: brightness,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.green,
+        brightness: brightness,
+      ),
     );
   }
 }
--- lib/routes/session_scoring.dart
+++ lib/routes/session_scoring.dart
@@ -6,25 +6,29 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'session_scoring.freezed.dart';
 
 final possibleScores = [
-  Score(label: 'X', color: Colors.yellow),
-  Score(label: '10', color: Colors.yellow),
-  Score(label: '9', color: Colors.yellow),
-  Score(label: '8', color: Colors.red),
-  Score(label: '7', color: Colors.red),
-  Score(label: '6', color: Colors.blue),
-  Score(label: '5', color: Colors.blue),
-  Score(label: '4', color: Colors.black),
-  Score(label: '3', color: Colors.black),
-  Score(label: '2', color: Colors.white),
-  Score(label: '1', color: Colors.white),
-  Score(label: 'M', color: Colors.green),
+  Score(label: 'X', value: 10, color: Colors.yellow),
+  Score(label: '10', value: 10, color: Colors.yellow),
+  Score(label: '9', value: 9, color: Colors.yellow),
+  Score(label: '8', value: 8, color: Colors.red),
+  Score(label: '7', value: 7, color: Colors.red),
+  Score(label: '6', value: 6, color: Colors.blue),
+  Score(label: '5', value: 5, color: Colors.blue),
+  Score(label: '4', value: 4, color: Colors.black),
+  Score(label: '3', value: 3, color: Colors.black),
+  Score(label: '2', value: 2, color: Colors.white),
+  Score(label: '1', value: 1, color: Colors.white),
+  Score(label: 'M', value: 0, color: Colors.green),
 ];
 
 @freezed
 abstract class Score with _$Score {
   const Score._();
 
-  const factory Score({required String label, required Color color}) = _Score;
+  const factory Score({
+    required String label,
+    required int value,
+    required Color color,
+  }) = _Score;
 
   Color get foregroundColor =>
       color.computeLuminance() > 0.5 ? Colors.black : Colors.white;
@@ -51,37 +55,8 @@ class _SessionScoringPageState extends State<SessionScoringPage> {
               top: false,
               bottom: false,
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: ListView(
-                  children: scores
-                      .slices(3)
-                      .map(
-                        (end) => Row(
-                          children: end
-                              .map(
-                                (score) => Container(
-                                  width: 40,
-                                  height: 40,
-                                  margin: EdgeInsets.all(4),
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: score.color,
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      score.label,
-                                      style: TextStyle(
-                                        color: score.foregroundColor,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              )
-                              .toList(),
-                        ),
-                      )
-                      .toList(),
-                ),
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: _ScoreSheet(scores: scores),
               ),
             ),
           ),
@@ -108,6 +83,82 @@ class _SessionScoringPageState extends State<SessionScoringPage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class _ScoreSheet extends StatelessWidget {
+  const _ScoreSheet({required this.scores});
+
+  final List<Score> scores;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        for (final (i, end) in scores.slices(6).indexed) ...[
+          Divider(),
+          _ScoreSheetRow(index: i + 1, scores: end),
+        ],
+
+        if (scores.isNotEmpty) Divider(),
+      ],
+    );
+  }
+}
+
+class _ScoreSheetRow extends StatelessWidget {
+  const _ScoreSheetRow({required this.index, required this.scores});
+
+  final int index;
+  final List<Score> scores;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Row(
+      children: [
+        Container(
+          width: 42,
+          padding: EdgeInsets.only(left: 8, right: 16),
+          child: Text(
+            '$index',
+            textAlign: TextAlign.end,
+            style: theme.textTheme.labelMedium!.copyWith(
+              color: theme.textTheme.labelMedium!.color!.withAlpha(150),
+            ),
+          ),
+        ),
+
+        for (final score in scores)
+          Container(
+            width: 36,
+            height: 36,
+            margin: EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: score.color,
+            ),
+            child: Center(
+              child: Text(
+                score.label,
+                style: TextStyle(color: score.foregroundColor),
+              ),
+            ),
+          ),
+
+        Spacer(),
+
+        Padding(
+          padding: const EdgeInsets.only(right: 8),
+          child: Text(
+            scores.map((s) => s.value).sum.toString(),
+            style: theme.textTheme.bodyLarge!.copyWith(
+              color: theme.colorScheme.primary,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
