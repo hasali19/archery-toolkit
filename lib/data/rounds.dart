1a4f078ff2b09a655fc02060851ce94dd021bb43
Hasan Mohsin <hasan@hasali.dev>
2025-09-28 08:59:21 +0100

    Add portsmouth round spec

--- lib/data/rounds.dart
+++ lib/data/rounds.dart
@@ -2,6 +2,19 @@ import 'package:archery_toolkit/core/models.dart';
 import 'package:archery_toolkit/data/scoring.dart';
 
 final standardRounds = {
+  'portsmouth': RoundDetails(
+    displayName: 'Portsmouth',
+    scoringSystem: ScoringSystems.metric,
+    distances: [
+      RoundDistance(
+        distanceValue: DistanceValue(20, DistanceUnit.yards),
+        arrowsPerEnd: 3,
+        ends: 20,
+        firstArrowIndex: 0,
+        firstEndIndex: 0,
+      ),
+    ],
+  ),
   'short_metric_1': RoundDetails(
     displayName: "Short Metric I",
     scoringSystem: ScoringSystems.metric,
