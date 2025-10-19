e9a3beadf5e03c5a434da3ef2328e68086dd819c
Hasan Mohsin <hasan@hasali.dev>
2025-10-19 10:08:00 +0100

    Update miss colour for worcester

--- lib/data/scoring.dart
+++ lib/data/scoring.dart
@@ -32,7 +32,7 @@ class ScoringSystems {
       Score(id: 3, label: '3', value: 3, color: Colors.black),
       Score(id: 4, label: '2', value: 2, color: Colors.black),
       Score(id: 5, label: '1', value: 1, color: Colors.black),
-      Score(id: 6, label: 'M', value: 0, color: Colors.black),
+      Score(id: 6, label: 'M', value: 0, color: Colors.green),
     ],
   );
 }
